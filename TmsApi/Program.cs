2332b5e1f53dc4635aa9e7ee23575706759670d1
BitanyYa <bethanywondimagegn@gmail.com>
2026-07-22 07:33:58 -0700

    Initial TMS API project setup

---
+++ TmsApi/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Authentication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services
+    .AddAuthentication("TrainingScheme")
+    .AddScheme<AuthenticationSchemeOptions, TrainingAuthHandler>(
+        "TrainingScheme",
+        options => { });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
