039bf6636484b47dfb5b566413e10451d7ed20cf
Imran <imrannuru290@gmail.com>
2026-08-03 12:02:08 +0300

    EF and models

---
+++ TmsApi/Models/Enrollment.cs
@@ -0,0 +1,12 @@
+namespace TmsApi.Models;
+
+public class Enrollment
+{
+    public int Id { get; set; }
+    public int StudentId { get; set; }
+    public Student Student { get; set; } = null!;
+    public int CourseId { get; set; }
+    public Course course { get; set; } = null!;
+    public DateTime EnrolledAt { get; set; }
+    public decimal? Grade { get; set; }
+}
---
+++ TmsApi/Models/Student.cs
@@ -0,0 +1,15 @@
+using TmsApi.Configuration;
+
+namespace TmsApi.Models;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string RegistrationNumber{ get; set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
+    public decimal GPA { get; set; }
+    public bool IsActive { get; set; }
+
+    public ICollection<Enrollment>Enrollments { get; set; } = new List<Enrollment>();
+
+}
